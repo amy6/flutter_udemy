5dfffce04d976a4d28f97d9b0c83b4e4862058ab
Mahima <mahima.6.ms@gmail.com>
2019-08-28 11:51:53 +0530

    complete the challenges for bitcoin-ticker

--- bitcoin-flutter-challenge-start/lib/coin_data.dart
+++ bitcoin-flutter-challenge-start/lib/coin_data.dart
@@ -1,4 +1,7 @@
 //TODO: Add your imports here.
+import 'dart:convert' as json;
+
+import 'package:http/http.dart' as http;
 
 const List<String> currenciesList = [
   'AUD',
@@ -31,8 +34,24 @@ const List<String> cryptoList = [
 ];
 
 const bitcoinAverageURL =
-    'https://apiv2.bitcoinaverage.com/indices/global/ticker';
+    'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
 
 class CoinData {
   //TODO: Create your getCoinData() method here.
+
+  Future<double> getCoinData(String currency, String cryptoType) async {
+    String url = bitcoinAverageURL + cryptoType + currency;
+    print("URL: $url");
+
+    double lastPrice;
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var jsonResponse = json.jsonDecode(response.body);
+      print("Response: $jsonResponse");
+      lastPrice = jsonResponse['last'];
+    }
+
+    return lastPrice;
+  }
 }
--- bitcoin-flutter-challenge-start/lib/price_screen.dart
+++ bitcoin-flutter-challenge-start/lib/price_screen.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'coin_data.dart';
 import 'dart:io' show Platform;
+
+import 'package:bitcoin_ticker/coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -9,7 +10,49 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  CoinData coinData = CoinData();
+
   String selectedCurrency = 'USD';
+
+  String btcPrice = '?';
+  String ethPrice = '?';
+  String ltcPrice = '?';
+
+  Widget getCryptoWidgets() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        cryptoCard(cryptoList[0], btcPrice.toString()),
+        cryptoCard(cryptoList[1], ethPrice.toString()),
+        cryptoCard(cryptoList[2], ltcPrice.toString()),
+      ],
+    );
+  }
+
+  Widget cryptoCard(String cryptoCurrency, String price) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            //TODO: Update the Text Widget with the live bitcoin data here.
+            '1 $cryptoCurrency = $price $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
@@ -27,6 +70,7 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
+          getData(selectedCurrency);
         });
       },
     );
@@ -49,11 +93,29 @@ class _PriceScreenState extends State<PriceScreen> {
   }
 
   //TODO: Create a method here called getData() to get the coin data from coin_data.dart
+  void getData(String selectedCurrency) async {
+    setState(() {
+      btcPrice = '?';
+      ethPrice = '?';
+      ltcPrice = '?';
+    });
+    var lastBtcPrice = await coinData.getCoinData(selectedCurrency, cryptoList[0]);
+    var lastEthPrice =
+        await coinData.getCoinData(selectedCurrency, cryptoList[1]);
+    var lastLtcPrice =
+        await coinData.getCoinData(selectedCurrency, cryptoList[2]);
+    setState(() {
+      btcPrice = lastBtcPrice.toString();
+      ethPrice = lastEthPrice.toString();
+      ltcPrice = lastLtcPrice.toString();
+    });
+  }
 
   @override
   void initState() {
     super.initState();
     //TODO: Call getData() when the screen loads up.
+    getData(selectedCurrency);
   }
 
   @override
@@ -68,25 +130,7 @@ class _PriceScreenState extends State<PriceScreen> {
         children: <Widget>[
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  //TODO: Update the Text Widget with the live bitcoin data here.
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+            child: getCryptoWidgets(),
           ),
           Container(
             height: 150.0,
